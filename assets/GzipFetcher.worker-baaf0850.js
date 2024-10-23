c7725bbe121a8eb9a3535ceb981204e37091a742
Billy Charlton <mail@billyc.cc>
2024-10-23 13:39:08 +0200

    gh-pages

---
+++ assets/GzipFetcher.worker-baaf0850.js
@@ -0,0 +1,2 @@
+function r(){return new Worker("/site/assets/GzipFetcher.worker-b7f6b2fd.js")}export{r as W};
+//# sourceMappingURL=GzipFetcher.worker-baaf0850.js.map
---
+++ assets/NewXmlFetcher.worker-4728f524.js
@@ -0,0 +1,2 @@
+function e(){return new Worker("/site/assets/NewXmlFetcher.worker-9c266e97.js")}export{e as W};
+//# sourceMappingURL=NewXmlFetcher.worker-4728f524.js.map
